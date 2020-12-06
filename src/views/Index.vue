47f647b3e2c2073711f693e9a63690fbf69abd58
valentin sld <valentin.salaud@gmail.com>
2020-12-06 13:54:27 +0100

    init repo

---
+++ src/views/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home test">
+  </div>
+</template>
+
+<script>
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Index',
+  // components: {
+  //   HelloWorld
+  // }
+}
+</script>
