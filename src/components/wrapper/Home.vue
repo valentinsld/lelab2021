c0bd6c6d4f50cd8090f78c8356ec988e722ec83b
valentin sld <valentin.salaud@gmail.com>
2020-12-19 17:15:38 +0100

    modified:   src/components/wrapper/Home.less 	modified:   src/components/wrapper/Home.vue 	modified:   src/components/wrapper/Wrapper.vue 	new file:   src/components/wrapper/WrapperTease.vue

--- src/components/wrapper/Home.vue
+++ src/components/wrapper/Home.vue
@@ -1,19 +1,50 @@
 <template>
     <div class="home">
-        <p class="home__text">
+        <p class="home__text" ref="text">
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
             nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
             erat, sed diam voluptua. At vero eos et accusam.
         </p>
-        <p class="home__scroll">Scroll</p>
+        <p class="home__scroll" ref="scroll">Scroll</p>
     </div>
 </template>
 
 <script>
 import './Home.less'
 
+import gsap from 'gsap'
+import SplitText from '@/assets/js/greensock/utils/SplitText'
+
+gsap.registerPlugin(SplitText)
+
 export default {
     name: 'Home',
-    mounted() {},
+    mounted() {
+        const textWords = new SplitText(this.$refs.text, {
+            type: 'words',
+            wordsClass: 'word',
+        }).words
+        const scrollChars = new SplitText(this.$refs.scroll, {
+            type: 'chars',
+            charsClass: 'char',
+        }).chars
+
+        const tl = gsap.timeline()
+
+        tl.to(textWords, {
+            delay: 6.7,
+            y: 0,
+            opacity: 1,
+            ease: 'Power4.out',
+            duration: 0.7,
+            stagger: 0.025,
+        }).to(scrollChars, {
+            y: 0,
+            opacity: 1,
+            ease: 'Power3.out',
+            duration: 0.5,
+            stagger: 0.1
+        })
+    },
 }
 </script>
---
+++ src/components/wrapper/WrapperTease.vue
@@ -0,0 +1,31 @@
+<template>
+    <div
+        class="wrapper__content"
+        ref="wrapper__content"
+        :style="{ width: width + 'px', transform }"
+    >
+        <Home />
+        <div class="div1"></div>
+        <div class="div2"></div>
+    </div>
+</template>
+
+<script>
+import Home from './Home.vue'
+
+export default {
+    name: 'WrapperTease',
+    props: ['width', 'transform'],
+    components: {
+        Home,
+    },
+    methods: {
+        getWidth() {
+            return this.$el.scrollWidth
+        },
+        setTransform(transformation) {
+            this.$el.style.transfom = transformation
+        },
+    },
+}
+</script>
