0cde5ea1bca18f468f8de4e39cb9802d77d50aa0
valentin sld <valentin.salaud@gmail.com>
2021-01-28 18:01:20 +0100

    init vue config for deploy

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+// vue.config.js
+const path = require('path')
+
+module.exports = {
+    chainWebpack: config => {
+        config.resolve.alias
+            .set('@', path.resolve(__dirname, './src'));
+  }
+}
