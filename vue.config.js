744d8c727706a733cc94d876d10d770c857d76ca
valentin sld <valentin.salaud@gmail.com>
2021-01-28 18:11:55 +0100

    test bis

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,6 @@ const path = require('path')
 module.exports = {
     chainWebpack: config => {
         config.resolve.alias
-            .set('@', path.resolve(__dirname, './src'));
+            .set('@', path.resolve(__dirname, 'src/'));
   }
 }
